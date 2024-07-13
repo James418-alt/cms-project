c9ddc8f047c0bde2a73167b60413c3fef1aa99e5
James418-alt <ebifegha123@gmail.com>
2024-07-13 01:45:19 +0100

    updated mongoDb

--- app/utils/dbConfig.ts
+++ app/utils/dbConfig.ts
@@ -1,7 +1,8 @@
 import { connect } from "mongoose";
 
 export const dbConfig = async () => {
-  const url = "mongodb://localhost:27017/cmsDb";
+  const url =
+    "mongodb+srv://ebifegha123:ebifegha123@gomenticode.1l9lbmv.mongodb.net/cmsDb?retryWrites=true&w=majority&appName=GomentiCode";
   await connect(url).then(() => {
     console.clear();
     console.log("Server Up!");
